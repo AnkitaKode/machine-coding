759f55a3a1cbc0b1d093686d7c58097d0de665e8
Ankita Kumari <ankita00iv@gmail.com>
2026-09-01 01:19:07 +0530

    initial commit

---
+++ 01-oops/Inheritance.java
@@ -0,0 +1,103 @@
+
+/*
+child object = Parents characters & behaviours + additional characters/behaviours
+Although objects of these child classes can 
+access or call parent class characters(variables) and behaviours(methods).
+Hence providing code reusability.
+*/
+
+class Car {
+    protected String brand;
+    protected String model;
+    protected boolean isEngineOn;
+    protected int currentSpeed;
+
+    public Car(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+        this.isEngineOn = false;
+        this.currentSpeed = 0;
+    }
+
+    // Common methods for all cars
+    public void startEngine() {
+        isEngineOn = true;
+        System.out.println(brand + " " + model + " : Engine started.");
+    }
+
+    public void stopEngine() {
+        isEngineOn = false;
+        currentSpeed = 0;
+        System.out.println(brand + " " + model + " : Engine turned off.");
+    }
+
+    public void accelerate() {
+        if (!isEngineOn) {
+            System.out.println(brand + " " + model + " : Cannot accelerate! Engine is off.");
+            return;
+        }
+        currentSpeed += 20;
+        System.out.println(brand + " " + model + " : Accelerating to " + currentSpeed + " km/h");
+    }
+
+    public void brake() {
+        currentSpeed -= 20;
+        if (currentSpeed < 0) currentSpeed = 0;
+        System.out.println(brand + " " + model + " : Braking! Speed is now " + currentSpeed + " km/h");
+    }
+}
+
+class ManualCar extends Car {
+    
+    private int currentGear;
+
+    public ManualCar(String brand, String model) {
+        super(brand, model);
+        this.currentGear = 0;
+    }
+
+    // Special method for Manual Car
+    public void shiftGear(int gear) {
+        this.currentGear = gear;
+        System.out.println(brand + " " + model + " : Shifted to gear " + currentGear);
+    }
+}
+
+
+class ElectricCar extends Car {
+    
+    private int batteryLevel;
+
+    public ElectricCar(String brand, String model) {
+        super(brand, model);
+        this.batteryLevel = 100;
+    }
+
+    // Special method for Electric Car
+    public void chargeBattery() {
+        batteryLevel = 100;
+        System.out.println(brand + " " + model + " : Battery fully charged!");
+    }
+}
+
+
+public class Inheritance {
+    public static void main(String[] args) {
+
+        ManualCar myManualCar = new ManualCar("Suzuki", "WagonR");
+        myManualCar.startEngine();
+        myManualCar.shiftGear(1); 
+        myManualCar.accelerate();
+        myManualCar.brake();
+        myManualCar.stopEngine();
+
+        System.out.println("-----------------------------");
+
+        ElectricCar myElectricCar = new ElectricCar("Tesla", "Model S");
+        myElectricCar.chargeBattery(); 
+        myElectricCar.startEngine();
+        myElectricCar.accelerate();
+        myElectricCar.brake();
+        myElectricCar.stopEngine();
+    }
+}
